b624f59548083cb6f9784c125dab432c51573474
Pratik Prajapati <pratik.prajapati@gmail.com>
2020-09-11 19:18:54 -0700

    Add extract_images_from_video
    exracts images from video
    
    Signed-off-by: Pratik Prajapati <pratik.prajapati@gmail.com>

--- data_utils/utils.py
+++ data_utils/utils.py
@@ -21,6 +21,22 @@ def create_video_from_images(images, output_video_filename, fps=30, res=(1920, 1
     video.release()
 
 
+def extract_images_from_video(input_video_filename, output_folder, res=None):
+    os.makedirs(output_folder, exist_ok=True)
+    capture = cv2.VideoCapture(input_video_filename)
+    frames_num = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for i in range(frames_num):
+        capture.grab()
+        success, frame = capture.retrieve()
+        if not success:
+            continue
+        out_image_name = os.path.join(output_folder, "{}.jpg".format(i))
+        if res is not None:
+            frame = cv2.resize(frame, res, interpolation=cv2.INTER_AREA)
+        cv2.imwrite(out_image_name, frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
+
+
 def locate_face_in_videofile(input_filepath=None, outfile_filepath=None):
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
