c609c3986d52a86f01b396b4da3f22f49ba6b345
Pratik Prajapati <pratik.prajapati@gmail.com>
2020-10-10 23:06:41 -0700

    update main with command line to resume training or start from scratch
    Signed-off-by: Pratik Prajapati <pratik.prajapati@gmail.com>

--- detect_deepfake.py
+++ detect_deepfake.py
@@ -2,19 +2,18 @@ import argparse
 from models.training_testing import *
 
 
-
-
 def main():
     print_banner()
-    train_model()
+    train_model(args.train_method)
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Data pre-processing for DFDC')
 
-    parser.add_argument('--apply_aug_to_sample', action='store_true',
-                        help='Apply augmentation and distractions to a file',
-                        default=False)
+    parser.add_argument('--train', dest='train_method', choices=['from_scratch', 'resume'],
+                        help='Train the model')
 
+    args = parser.parse_args()
+    print(args)
     main()
-
