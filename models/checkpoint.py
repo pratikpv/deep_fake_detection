27fef763b10119edaec51a3ca7eabe8190d35fdf
Pratik Prajapati <pratik.prajapati@gmail.com>
2020-10-24 11:09:46 -0800

    update checkpoint loading with new interface
    Signed-off-by: Pratik Prajapati <pratik.prajapati@gmail.com>

--- models/checkpoint.py
+++ models/checkpoint.py
@@ -23,13 +23,12 @@ def save_checkpoint(epoch=None, model=None, model_params=None,
     torch.save(check_point_dict, check_point_path)
 
 
-def load_checkpoint(model=None, optimizer=None):
+def load_checkpoint(model=None, optimizer=None, check_point_path=None):
     model_class_name = type(model).__name__
-    check_point_path = os.path.join(get_checkpoint_root_path(), model_class_name + '.chkpt')
     check_point_dict = torch.load(check_point_path)
 
     if check_point_dict['model_class_name'] != model_class_name:
         raise Exception('Invalid checkpoint loading requested')
     model.load_state_dict(check_point_dict['model_state_dict'])
     optimizer.load_state_dict(check_point_dict['optimizer_state_dict'])
-    return check_point_dict['epoch'], model, optimizer
+    return check_point_dict['epoch'], model, optimizer, check_point_dict['model_params'], check_point_dict['log_dir']
