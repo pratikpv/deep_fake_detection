80dcd5ddd67e0fae8465f79f2b5388da137300be
Pratik Prajapati <pratik.prajapati@gmail.com>
2020-10-27 21:04:02 -0700

    little comments and minor fix for model5
    Signed-off-by: Pratik Prajapati <pratik.prajapati@gmail.com>

--- models/DeepFakeDetectModel_5.py
+++ models/DeepFakeDetectModel_5.py
@@ -8,6 +8,25 @@ import torch.nn.functional as F
 
 
 class DeepFakeDetectModel_5(nn.Module):
+    """
+    max_num_frames are passed stacked.
+    For each frame, features are extracted. Features are flatten and passed as a
+    single feature-map to classifier.
+
+    Expects original method of training/testing with DFDCDataset class
+
+    sample valid params for config.yml
+    params:
+        model_name: 'DeepFakeDetectModel_5'
+        max_num_frames: 5
+        batch_size: 32
+        random_sorted: True
+        expand_label_dim: False
+        epochs: 50
+        learning_rate: 0.001
+        fill_empty: True
+
+    """
     def __init__(self, frame_dim=None, max_num_frames=5, encoder_name=None):
         super().__init__()
         self.image_dim = frame_dim
@@ -21,15 +40,19 @@ class DeepFakeDetectModel_5(nn.Module):
             nn.Linear(classifier_in, classifier_hidden),
             nn.Dropout(0.50),
             nn.ReLU(),
-            nn.Linear(classifier_hidden, 2)
+            nn.Linear(classifier_hidden, self.num_of_classes)
         )
 
     def forward(self, x):
+        # x.shape = batch_size x max_num_frames x color_channel x image_h x image_w
         batch_size = x.shape[0]
         num_frames = x.shape[1]
         features = []
         # extract features of each frame and save them in features list, process batch-wise
         for b in range(batch_size):
+            # x[b].shape = max_num_frames x color_channel x image_h x image_w
+            # in passing x[b] to encoder.forward_features(),  max_num_frames acts as batch_size for the video
+            # as passing number of frames in batches.
             f = self.encoder.forward_features(x[b])
             f = f.flatten()
             features.append(f)
