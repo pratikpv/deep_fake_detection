ca81408b43516f864fdc81483ed24dfc9313ad47
Pratik Prajapati <pratik.prajapati@gmail.com>
2020-10-31 01:45:32 -0800

    use Adaptive avg polling
    Signed-off-by: Pratik Prajapati <pratik.prajapati@gmail.com>

--- models/DeepFakeDetectModel_6.py
+++ models/DeepFakeDetectModel_6.py
@@ -20,12 +20,15 @@ class DeepFakeDetectModel_6(nn.Module):
         learning_rate: 0.001
         batch_size: 128
     """
+
     def __init__(self, frame_dim=None, encoder_name=None):
         super().__init__()
+
         self.image_dim = frame_dim
         self.num_of_classes = 1
         self.encoder = encoder_params[encoder_name]["init_op"]()
         self.encoder_flat_feature_dim = encoder_params[encoder_name]["flat_features_dim"]
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
 
         self.classifier = nn.Sequential(
             nn.Linear(self.encoder_flat_feature_dim, int(self.encoder_flat_feature_dim * .10)),
@@ -36,8 +39,7 @@ class DeepFakeDetectModel_6(nn.Module):
 
     def forward(self, x):
         # x shape = batch_size x color_channels x image_h x image_w
-        batch_size = x.shape[0]
         x = self.encoder.forward_features(x)
-        x = x.view(batch_size, -1)
+        x = self.avg_pool(x).flatten(1)
         x = self.classifier(x)
         return x
