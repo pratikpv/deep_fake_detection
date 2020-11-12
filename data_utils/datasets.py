b4b117277c1cb832134facc134a4fd5ab6c348ef
Pratik Prajapati <pratik.prajapati@gmail.com>
2020-11-11 18:46:39 -0800

    Dataset for Xray of the images
    Signed-off-by: Pratik Prajapati <pratik.prajapati@gmail.com>

--- data_utils/datasets.py
+++ data_utils/datasets.py
@@ -159,3 +159,42 @@ class DFDCDatasetSimple(Dataset):
                 return item
             except Exception:
                 index = random.randint(0, self.data_len)
+
+
+class MRIDataset(Dataset):
+    def __init__(self, transforms=None, mode="train"):
+        self.transforms = transforms
+
+        if mode == "train":
+            self.data_csv = get_xray_pairs_train_csv()
+        elif mode == "test":
+            self.data_csv = get_xray_pairs_test_csv()
+        else:
+            raise Exception("Unknown mode")
+
+        self.df = pd.read_csv(self.data_csv)
+        self.data_dict = self.df.to_dict(orient='records')
+        self.df_len = len(self.df)
+
+    def __getitem__(self, index):
+        while True:
+            try:
+                item = self.data_dict[index].copy()
+                img_A = Image.open(str(item['face_image']))
+                img_B = Image.open(str(item['xray_image']))
+
+                if np.random.random() < 0.5:
+                    img_A = Image.fromarray(np.array(img_A)[:, ::-1, :], "RGB")
+                    img_B = Image.fromarray(np.array(img_B)[:, ::-1, :], "RGB")
+
+                if self.transforms:
+                    img_A = self.transforms(img_A)
+                    img_B = self.transforms(img_B)
+
+                return {"A": img_A, "B": img_B}
+
+            except Exception:
+                index = random.randint(0, self.df_len)
+
+    def __len__(self) -> int:
+        return self.df_len
