de32fc7d3fdf6a5d3dc33cb528b7add313713daa
Pratik Prajapati <pratik.prajapati@gmail.com>
2020-11-17 19:46:55 -0800

    Dataset for Pix2Pix GAN
    Signed-off-by: Pratik Prajapati <pratik.prajapati@gmail.com>

--- data_utils/datasets.py
+++ data_utils/datasets.py
@@ -198,3 +198,22 @@ class MRIDataset(Dataset):
 
     def __len__(self) -> int:
         return self.df_len
+
+
+class SimpleImageFolder(Dataset):
+    def __init__(self, root, transforms_=None):
+        self.root = root
+        all_files = glob(root + "/*.png")
+        self.data_list = [os.path.abspath(f) for f in all_files]
+        self.data_len = len(self.data_list)
+        self.transforms = transforms_
+
+    def __getitem__(self, index):
+        img_name = self.data_list[index]
+        img = Image.open(self.data_list[index])
+        if self.transforms:
+            img = self.transforms(img)
+        return img, img_name
+
+    def __len__(self) -> int:
+        return self.data_len
